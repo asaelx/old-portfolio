6a6209b3dde557c095e084d5f276653651377e1e
Asael Chavez <asael.chavez@interalia.net>
2014-12-09 13:31:27 -0600

    Reset css

--- app/views/base/base.blade.php
+++ app/views/base/base.blade.php
@@ -5,6 +5,7 @@
     <meta name="author" content="Asael Jaimes">
     <meta name="description" content="Portafolio personal de Asael Jaimes">
     <title>Asael Jaimes - @yield('title')</title>
+    {{HTML::style('assets/css/reset.css')}}
 </head>
 <body>
 
