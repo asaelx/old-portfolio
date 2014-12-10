299042e3aae691ed4317f3ef5ffa3be3a5a2d750
Asael Chavez <asaelx@gmail.com>
2014-12-09 23:32:32 -0600

    Added Twitter API to Composer

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Barryvdh\VendorCleanup\VendorCleanupServiceProvider',
+		'Thujohn\Twitter\TwitterServiceProvider',
 
 	),
 
@@ -189,6 +190,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
+		'Twitter' => 'Thujohn\Twitter\TwitterFacade',
 
 	),
 
