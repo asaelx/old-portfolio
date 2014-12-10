31cc0e905e4c1e6902a82bedbb8a7085f1c43f3b
Asael Chavez <asaelx@gmail.com>
2014-12-09 21:37:07 -0600

    Show/Hide Navigation menu and limit instagram feed to 15 photos

--- assets/js/magic.js
+++ assets/js/magic.js
@@ -3,6 +3,6 @@ var feed = new Instafeed({
     userId: 371921,
     accessToken: '371921.467ede5.a093aec36e6a40619a1c40e40edc9837',
     template: '<li class="option"><a class="link" href="{{link}}" target="_blank"><img src="{{image}}" class="img"></a></li>',
-    limit: 18
+    limit: 15
 });
 feed.run();
